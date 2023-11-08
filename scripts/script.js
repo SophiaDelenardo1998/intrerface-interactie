3c92db22e85db95fc9994d03c284cb940234c7b0
SophiaDelenardo1998 <70689522+SophiaDelenardo1998@users.noreply.github.com>
2023-11-08 14:32:02 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,105 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var gameP = document.querySelector("main > section:nth-of-type(2) section:nth-of-type(1) p");
+var gameh3 = document.querySelector("main > section:nth-of-type(2) section:nth-of-type(1) h3");
+var titlegameh2 = document.querySelector("h2");
+
+var gamePblok2 = document.querySelector("main > section:nth-of-type(2) section:nth-of-type(2) p");
+var gameh3blok3 = document.querySelector("main > section:nth-of-type(2) section:nth-of-type(2) h3");
+
+
+var zelda2001 = document.querySelector("li:nth-of-type(1) button");
+
+
+zelda2001.onclick = toonInfoGame1;
+
+
+function toonInfoGame1(){
+    gameP.innerHTML = "Gebaseerd op de game Ocorina of time. Het spel werd oorspronkelijk uitgebracht voor de Nintendo 64 in 1998 en wordt beschouwd als een van de meest invloedrijke en gerespecteerde titels in de geschiedenis van videogames. Een van de meest opvallende elementen van het spel is de Ocarina, een magisch muziekinstrument dat Link gebruikt om melodieën te spelen om de wereld te veranderen en toegang te krijgen tot nieuwe gebieden.";
+    titlegameh2.innerHTML = "Super Smash Bros. Melee: 2001";
+    gameh3.innerHTML = "The legend of zelda: ocarina of time ";
+    gamePblok2.innerHTML  = "Super Smash Bros. Melee 2001: Princess Zelda in Melee geen specifieke Final Smash of superkracht zoals in latere spellen in de serie. In plaats daarvan heeft ze verschillende speciale aanvallen en bewegingen die ze kan gebruiken in gevechten tegen andere personages. ";
+    gameh3blok3.innerHTML = "Super power van Zelda";
+}
+
+
+var zelda2008 = document.querySelector("li:nth-of-type(2) button");
+
+zelda2008.onclick = toonInfoGame2;
+
+function toonInfoGame2(){
+    gameP.innerHTML = "Het karakter is gebaseerd op de game Ocorina of time. In The Legend of Zelda: Ocarina of Time voor de Nintendo 3DS, maakt Sheik zijn debuut als een belangrijk personage in het spel.  Sheik is een mysterieuze figuur die gedurende Link helpt op zijn quest.  Aan het einde van het spel kom je er achter wie Sheik echt is, namelijk Zelda.";
+    titlegameh2.innerHTML = "Super Smash Bros. Melee: 2001";
+    gameh3.innerHTML = "The Legend of Zelda: Ocarina of time (3ds)";
+    gamePblok2.innerHTML  = "Bouncing Fish (Zijwaartse Speciale Aanval),Needle Storm (Neutrale Speciale Aanval), Vanish (Omhoogwaartse Speciale Aanval), Chain (Omlaagwaartse Speciale Aanval). ";
+    gameh3blok3.innerHTML = "Super power van Sheik";
+}
+
+
+var zelda2008 = document.querySelector("li:nth-of-type(3) button");
+
+zelda2008.onclick = toonInfoGame3;
+
+function toonInfoGame3(){
+    gameP.innerHTML = "Zelda is gebaseerd op Twilight princess. Twilight Princess onderscheidt zich van de eerdere Zelda-games door de meer realistische en duistere grafische stijl";
+    titlegameh2.innerHTML = "2008 Super Smash Bros. Brawl - Zelda";
+    gameh3.innerHTML = "The legend of zelda twilight princess";
+    gamePblok2.innerHTML  = "In Super Smash Bros. Brawl 2008, heeft Princess Zelda (die ook Sheik kan worden) een speciale aanval genaamd Light Arrow als haar Final Smash, wat haar superaanval is. Wanneer Zelda haar Final Smash uitvoert, trekt ze de Bow of Light en vuurt een krachtige lichtpijl af, die schade toebrengt aan vijand spelers.";
+    gameh3blok3.innerHTML = "Super power van Zelda";
+}
+
+var sheik2008 = document.querySelector("li:nth-of-type(4) button");
+
+sheik2008.onclick = toonInfoGame4;
+
+function toonInfoGame4(){
+    gameP.innerHTML = "Het design van Sheik is gebaseerd op Twilight princess, maar ze komt daar niet in voor. Dit is gedaan zodat het match met de Twilight princess Zelda. Sheik en Zelda zijn in de game Super Smash Bros.Brawl samen als een karakter.";
+    titlegameh2.innerHTML = "2008 Super Smash Bros. Brawl";
+    gameh3.innerHTML = "The legend of zelda twilight princess";
+    gamePblok2.innerHTML  = "In Super Smash Bros. Brawl uit 2008 heeft Sheik geen Final Smash-aanval met de naam Light Arrow zoals Zelda. In plaats daarvan heeft Sheik een andere Final Smash genaamd Sheikah Dance. Wanneer Sheik haar Final Smash uitvoert, begint ze een snel en krachtig aanvalspatroon waarbij ze automatisch naar vijandelijke spelers sprint en herhaaldelijk snelle aanvallen uitvoert.";
+    gameh3blok3.innerHTML = "Super power van Sheik";
+
+}
+
+
+var zeldaUtimate = document.querySelector("li:nth-of-type(5) button");
+
+zeldaUtimate.onclick = toonInfoGame5;
+
+function toonInfoGame5(){
+    gameP.innerHTML = "Zelda karakter is in deze game gebaseerd op link to the past en link between worlds.Een van de meest opvallende kenmerken van de game is het huur- en koopsysteem voor wapens en voorwerpen. In plaats van wapens in dungeons te vinden, kunnen spelers ze huren of kopen van Ravio, een rondreizende koopman. Dit systeem geeft spelers meer vrijheid om te kiezen welke wapens ze willen gebruiken en in welke volgorde ze de dungeons willen verkennen.";
+    titlegameh2.innerHTML = "2018 Super Smash Bros. Ultimate";
+    gameh3.innerHTML = "The legend of zelda a link to the past & The Legend of Zelda: A Link Between Worlds";
+    gamePblok2.innerHTML  = "In Super Smash Bros. Ultimate uit 2018, heeft Princess Zelda haar eigen Final Smash-aanval genaamd Sealing Triforce.W anneer Zelda deze Final Smash uitvoert, roept ze een reusachtige Triforce-symbool op dat voor haar verschijnt. Als een tegenstander binnen het bereik van de Triforce komt, wordt deze vastgezet in een cirkel van licht. Nadat de tegenstanders zijn verzameld, onthult Zelda de Triforce en veroorzaakt een enorme explosie die aanzienlijke schade toebrengt aan iedereen binnen het effectgebied. Deze aanval heeft ook een grote knockback-kracht, wat betekent dat het tegenstanders van het speelveld kan stoten en hen potentieel kan elimineren. ";
+    gameh3blok3.innerHTML = "Super power van Zelda";
+}
+
+var sheikUtimate = document.querySelector("li:nth-of-type(6) button");
+
+sheikUtimate.onclick = toonInfoGame6;
+
+function toonInfoGame6(){
+    gameP.innerHTML = "Sheik in de game gebaseerd op het pak van Link die hij aan heeft in breath of the wild. The Legend of Zelda: Breath of the Wild onderscheidt zich van eerdere Zelda-games op verschillende manieren: open wereld, Vernieuwde gameplay-mechanica: je kunt namelijk nu op muren klimmen, koude- en hittebestendigheid.Het spel legt de nadruk op verkenning en ontdekking en zelf veel beslissingen nemen.";
+    titlegameh2.innerHTML = "2018 Super Smash Bros. Ultimate";
+    gameh3.innerHTML = "The legend of zelda breath of the wild";
+    gamePblok2.innerHTML  = "In Super Smash Bros. Ultimate uit 2018, heeft Sheik haar eigen Final Smash-aanval genaamd Sheikah Dance. Wanneer Sheik deze Final Smash uitvoert, begint ze een reeks snelle en krachtige aanvallen die gericht zijn op vijandelijke spelers binnen het bereik van de aanval. Ze beweegt zich razendsnel en valt herhaaldelijk aan met haar messen en speciale vaardigheden. Het is een aanval die moeilijk te ontwijken is en aanzienlijke schade kan toebrengen aan vijandelijke spelers. Sheik's Sheikah Dance Final Smash kan ook leiden tot knockouts als het goed wordt uitgevoerd.Dit is de Final Smash-aanval voor Sheik in Super Smash Bros. Ultimate, en het is een krachtig middel om vijandelijke spelers te verslaan en punten te scoren in het spel.";
+    gameh3blok3.innerHTML = "Super power van Sheik";
+}
+
+var buttonLink = document.querySelector("main>button");
+var Link = document.querySelector("main>img");
+
+buttonLink.onclick = gaRennenLink;
+
+
+/* ben met dit geholpen en snap het nu wel*/
+function gaRennenLink(){
+    Link.classList.toggle("ikRen");
+    if(Link.classList.contains("ikRen")){
+        setTimeout(() => {
+            Link.classList.remove("ikRen");
+        }, 2000);
+    }
+}
+
